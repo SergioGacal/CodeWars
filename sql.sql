f1160120c198552f282014f470a4e923e182a8f6
SergioGacal <gacalsergio@gmail.com>
2024-06-04 13:05:55 -0300

    Ejercios resueltos de SQL

--- sql.sql
+++ sql.sql
@@ -1,3 +1,6 @@
 SELECT CONCAT('Hello, ', name, ' how are you doing today?') AS greeting FROM person;
 
 SELECT * from people WHERE age > 50 ORDER BY age DESC;
+
+SELECT name, age from users where age >=18;
+
