819e133ffaf3f6c1efe9efb7084338b66ee60b60
SergioGacal <gacalsergio@gmail.com>
2024-07-25 09:32:40 -0300

    Ejercicios de python

--- python.py
+++ python.py
@@ -2992,3 +2992,18 @@ def greet(name):
 
 def _all(seq, fun): 
     return all(map(fun,seq))
+
+def number_to_pwr(number, p): 
+    resultado = 1
+    for i in range(p):
+        resultado *= number
+    return resultado
+
+def uefa_euro_2016(teams, scores):
+    if scores[0]>scores[1]:
+        return f'At match {teams[0]} - {teams[1]}, {teams[0]} won!' 
+    elif scores[0]<scores[1]:
+        return f'At match {teams[0]} - {teams[1]}, {teams[1]} won!'
+    else:
+        return f'At match {teams[0]} - {teams[1]}, teams played draw.'
+    
