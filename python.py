07572cc63185bf737ccd1913c530ff88a92b94f0
SergioGacal <gacalsergio@gmail.com>
2024-08-08 13:36:29 -0300

    Ejercicio py

--- python.py
+++ python.py
@@ -3071,3 +3071,6 @@ def folding(a, b):
 def whatday(num):
     dias = {1:'Sunday',2:'Monday',3:'Tuesday',4:'Wednesday',5:'Thursday',6:'Friday',7:'Saturday'}
     return dias.get(num) if num < 8 and num > 0 else 'Wrong, please enter a number between 1 and 7'
+
+def square_area(A):
+    return round((2 * A / math.pi) ** 2, 2)
