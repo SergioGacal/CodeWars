2a9cd38ffd9f71e4505e8adb1a66c48604da57c8
SergioGacal <gacalsergio@gmail.com>
2024-09-30 12:35:40 -0300

    Ejercicios python.

--- python.py
+++ python.py
@@ -3184,3 +3184,6 @@ def print_array(arr):
 
 def get_ascii(ch: str) -> int:
     return ord(ch)
+
+def elevator(left, right, call):
+    return "left" if abs(call - left) < abs(call - right) else "right"
